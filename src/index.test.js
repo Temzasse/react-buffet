331d93b534d2d0dfc3882765127e0e847c16dfbd
Teemu Taskula <teemu.taskula@aalto.fi>
2016-11-09 15:18:01 +0800

    chore(build): Add mock testing for travis
    Travis runs tests only once

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+/**
+ * React Buffet tests.
+ */
+
+var expect = require('chai').expect;
+
+describe('react-buffet', function() {
+  it('should just work', function() {
+    expect(true).to.be.true;
+  });
+});
