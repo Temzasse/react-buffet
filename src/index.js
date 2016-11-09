8879900762984ad1401365b817bb518bff7e7592
Teemu Taskula <teemu.taskula@aalto.fi>
2016-11-09 14:59:56 +0800

    chore(releasing): Added semantic-release and commitizen to help with releasing

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * React Buffet
+ * ------------
+ * A collection, "buffet", of reusable React components.
+ * Table is served - Enjoy! ;)
+ */
+
+module.exports = {
+  test: 'Testing',
+  another: 'Testing still',
+};
